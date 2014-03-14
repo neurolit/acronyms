1302a84b244383d3dd80df221d3b7e0443b952e9
Neurolit <neurolit@gmail.com>
2014-03-14 23:41:26 +0100

    Replace regexp by fuzzy searching (with Fuse.js)

--- js/controllers.js
+++ js/controllers.js
@@ -1,23 +1,29 @@
 function AcronymsSearchCtrl($scope, $http) {
     "use strict";
 
+    var options = {
+            keys: ['id', 'value']
+        },
+        fuse ; 
+
     $scope.filter = {};
     $scope.filter.value = "";
     $scope.acronyms = [];
 
     $http.get('data/list.json').success(function (data) {
-        $scope.acronyms = data.sort(function (a, b) { return a.id < b.id; });
+        $scope.acronyms = data.sort(function (a,b) { return a.id < b.id ; });
+        fuse = new Fuse($scope.acronyms, options);
     });
 
 
     $scope.getFilteredAcronyms = function () {
-        function compareAcronyms(value) {
-            var re = new RegExp(this.filter.value, "i");
-            return re.test(value.id) || re.test(value.value);
+        function compare(value) {
+            var re = new RegExp (this.filter.value,"i")  
+            return re.test(value.id);
         }
         if ($scope.filter.value === "") {
             return [];
         }
-        return $scope.acronyms.filter(compareAcronyms, $scope);
-    };
+        return fuse.search($scope.filter.value);
+    }
 }
