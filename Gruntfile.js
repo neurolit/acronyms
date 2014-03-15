98cb69dc43781603d129a2e337505c9c4d4ef136
Neurolit <neurolit@gmail.com>
2014-03-15 01:05:00 +0100

    Grunt: new task transforming acronyms dict into json

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,6 +23,46 @@ module.exports = function (grunt) {
 
     });
 
+    grunt.registerTask('dictd_to_json', function () {
+        var acronyms = [],
+            data = grunt.file.read('data/acroinria.src').split("\n"),
+            current_acronym = {},
+            in_header = true;
+        data.forEach(
+            function (line) {
+                if (line === "") {
+                    grunt.log.writeln("Empty line");
+                    current_acronym.value += "\n";
+                } else if (line.substring(0, 1) === "\t") {
+                    grunt.log.writeln("Tab");
+                    current_acronym.value += line.substring(1, line.length) + "\n";
+                } else if (line.substring(0, 1) === " " || line.substring(0, 1) === "-") {
+                    if (in_header) {
+                        grunt.log.writeln("Header " + line);
+                    } else {
+                        grunt.log.writeln("Spaces " + line);
+                        line.replace(/^\s+/, "");
+                        current_acronym.value += line + "\n";
+                    }
+                } else {
+                    grunt.log.writeln("Acronym");
+                    // Nouvel acronym
+                    if (in_header) {
+                        in_header = false;
+                    } else {
+                        acronyms.push(current_acronym);
+                    }
+                    current_acronym = {};
+                    current_acronym.id = line;
+                    current_acronym.value = "";
+                }
+            }
+        );
+        acronyms.push(current_acronym);
+        grunt.file.write("data/list.json", JSON.stringify(acronyms, null, 4));
+    });
+
+
     // default task.
     grunt.registerTask('default', ['jslint']);
 
